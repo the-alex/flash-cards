73d8630b91dea426973e7186437cc9f0447dd98f
Alex Chojnacki <thealex@umich.edu>
2015-05-30 19:44:54 -0400

    RAWR

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var create_deck = require('card-parser');
 router.get('/', function(req, res, next) {
   // For now, load default deck for testing ...
   // var deck = create_deck('/Users/theProfessional/Development/repositories/flash-cards/node_modules/card-parser/test1.txt');
-  var deck = create_deck('./../node_modules/card-parser/word_list_psych.md');
+  var deck = create_deck(process.cwd() + '/node_modules/card-parser/word_list_psych.md');
 
   var index = 0;
   // Get the index from the params if there was one.
